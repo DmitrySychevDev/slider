65fb9e7fdae36837c22a37ae005427ef6db8074d
Dmitry Sychev <dimtrisythev@gmail.com>
2022-08-03 20:47:03 +0600

    добавленна работа с точками

--- index.js
+++ index.js
@@ -8,18 +8,66 @@ class Slider{
             curentSlide:this._slidesList[0],
             nextSlide:this._slidesList[1],
         };
+        this._init_slides();
         this._transmitionIsComplete=true;
+        this._createDots();
+        this._dotsList=document.querySelectorAll(".dot");
     }
+    //======================================================
+    _createDots(){
+
+        for(let i=0;i<this._slidesList.length;i++)
+        {
+            let dot=document.createElement("button");
+            if(i==0) dot.classList.add("dot_active");
+            dot.classList.add("dot");
+            //.................................................................
+            dot.addEventListener("click",(e)=>{
+                if(this._transmitionIsComplete){
+                const dotIndex=this._getIndex(e.target,this._dotsList);
+                const curentIndex=this._getIndex(this._nearestSlides.curentSlide,this._slidesList);
+                let direction;
+                if(curentIndex>dotIndex){
+                    this._nearestSlides.prevSlide.style.display="none";
+                   this._nearestSlides.prevSlide=this._slidesList[dotIndex];
+                   this._nearestSlides.prevSlide.style.display="block";
+
+                   direction="left";
+                }
+    
+                if(curentIndex<dotIndex){
+                    this._nearestSlides.nextSlide.style.display="none";
+                    this._nearestSlides.nextSlide=this._slidesList[dotIndex];
+                    this._nearestSlides.nextSlide.style.display="block";
+                    direction="right";
+                }
+                this.slideTransmition(direction)();
+                this._setActiveDot(dotIndex);
+            }
+            });
+            this._dotsBlock.appendChild(dot);
+        }
+    }
+    
+    //=======================================================
+    _setActiveDot(index){
+        this._dotsList.forEach((dot)=>{
+            if(dot.classList.contains("dot_active")) dot.classList.remove("dot_active");
+        });
+        this._dotsList[index].classList.add("dot_active");
+    }
+    //================================================
     _getIndex(elem,arr){
         let searcIndex;
         arr.forEach((element,index)=>{
-            console.log(element)
             if(element===elem){ 
                 searcIndex=index;
             }
         });
         return searcIndex
     }
+    //=====================================================
+
     slideTransmition(direction){
         return ()=>{
         if(this._transmitionIsComplete){
@@ -28,6 +76,7 @@ class Slider{
         let interval=setInterval(()=>{
         switch(direction){
             case "right":
+                console.log("right");
                 leftOfset++;
                 this._nearestSlides.nextSlide.style.left=(105-leftOfset)+"%";
                 this._nearestSlides.curentSlide.style.left=(-leftOfset)+"%";
@@ -41,40 +90,38 @@ class Slider{
         if(leftOfset==105){
             this._transmitionIsComplete=true;   
             }
-                    //.....................................................
+        //............................................................................
         if(this._transmitionIsComplete){
             //переназначение слайдов
-            console.log("Переназначение")
+           
         if(direction=="right"){
-            this._nearestSlides.prevSlide=this._nearestSlides.curentSlide;
             this._nearestSlides.curentSlide=this._nearestSlides.nextSlide;
-            const nextIndex=this._getIndex(this._nearestSlides.nextSlide,this._slidesList);
-            console.log(nextIndex);
-            if(nextIndex==this._slidesNum-1){
-                this._nearestSlides.nextSlide=this._slidesList[0];
-            }
-            else this._nearestSlides.nextSlide=this._slidesList[nextIndex+1];
         }
         //...................................................
         if(direction=="left"){
-            this._nearestSlides.nextSlide=this._nearestSlides.curentSlide;
-            this._nearestSlides.curentSlide=this._nearestSlides.prevSlide;
-            const prevIndex=this._getIndex(this._nearestSlides.prevSlide,this._slidesList);
-            if(prevIndex==0){
-                this._nearestSlides.prevSlide=this._slidesList[this._slidesNum-1];
-            }
-            else this._nearestSlides.prevSlide=this._slidesList[prevIndex-1];
+                this._nearestSlides.curentSlide=this._nearestSlides.prevSlide;        
         }
-        this.init_slides();
+        //..............................................................................
+        const curentIndex=this._getIndex(this._nearestSlides.curentSlide,this._slidesList);
+        if(curentIndex==0){
+            this._nearestSlides.prevSlide=this._slidesList[this._slidesNum-1];
+        }
+        else this._nearestSlides.prevSlide=this._slidesList[curentIndex-1];
+        //..................................................................
+        if(curentIndex==this._slidesNum-1) this._nearestSlides.nextSlide=this._slidesList[0];
+        else this._nearestSlides.nextSlide=this._slidesList[curentIndex+1];
+        //..............................................
+        this._init_slides();
         clearInterval(interval);
-
+        this._setActiveDot(curentIndex);
         }
         },5);
 
     }
     }
 }
-    init_slides(){
+//============================================================================
+    _init_slides(){
         this._nearestSlides.curentSlide.style.left="0%";
         this._nearestSlides.nextSlide.style.left="105%";
         this._nearestSlides.prevSlide.style.left="-105%";
@@ -88,7 +135,6 @@ class Slider{
         });
     }
 }
-const sliderObj=new Slider(document.querySelectorAll(".slider__slide"),document.querySelectorAll(".dots-wraper"));
+const sliderObj=new Slider(document.querySelectorAll(".slider__slide"),document.querySelector(".dots-wraper"));
 document.querySelector(".prev").addEventListener("click",sliderObj.slideTransmition("left"));
 document.querySelector(".next").addEventListener("click",sliderObj.slideTransmition("right"));
-sliderObj.init_slides();
